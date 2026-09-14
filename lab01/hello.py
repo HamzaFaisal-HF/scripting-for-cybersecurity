8bcefb27f8056feeefe3ec39bbdcd1b1e8bd62c9
HamzaFaisal-HF <hamzaadasali@gmail.com>
2026-09-14 15:03:02 +0000

    .

--- lab01/hello.py
+++ lab01/hello.py
@@ -1,6 +1,6 @@
 name = input("What is your name? ")
 name2 = input("What do you study ")
-name3 = input("What are you intrested in " + name2)
+name3 = input("What is your favorite area in " + name2)
 
 print("Hello, " + name + "!")
 print("You are studying " + name2 + "!")
