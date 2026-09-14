6ee8778aa603b26b41c1decb8f468c50f8a5fa78
HamzaFaisal-HF <hamzaadasali@gmail.com>
2026-09-14 15:10:29 +0000

    .

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+name2 = input("What do you student number ")
+name3 = input("What year of study are you ")
+
+print("name: " +name)
+print("Student number: " +name2)
+print("Year: " +name3)
